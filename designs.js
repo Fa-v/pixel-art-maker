1755135c06f869946ee91d9b7f62bae41b6c1c63
Fabiola Vieyra <fabiolacvieyra@gmail.com>
2017-10-11 11:49:18 +0200

    Grabbing input values

--- designs.js
+++ designs.js
@@ -1,7 +1,21 @@
+$(document).ready(submitGridSize);
+
 // Select color input
+const colorInput = $('input[type=color]');
+
 // Select size input
+const height = $('input[type=number][name=height]');
+const width = $('input[type=number][name=width]');
 
 // When size is submitted by the user, call makeGrid()
+function submitGridSize() {
+  $('#button').click(function (event) {
+    event.preventDefault();
+    let rows = Number ($(height).val());
+    let columns = Number($(width).val());
+    let color = colorInput.val();
+  });
+}
 
 function makeGrid() {
 
